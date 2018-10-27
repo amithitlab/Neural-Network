6d270d97702b86396f55e9857fe37834265576fe
Amit Singh <amit8564880158@gmail.com>
2018-10-27 23:34:59 +0530

    Update Neural Network.py

--- Neural Network.py
+++ Neural Network.py
@@ -7,3 +7,14 @@ Created on Sat Oct 27 23:22:42 2018
 
 import numpy as np
 import pandas as pd
+
+x,y,w1,b1,w2,b2,w3,b3
+
+
+def forward(x):
+    z1=x.w1+b
+    a1=tanh(z1)
+    z2=a1.w2+b2
+    a2=tanh(z2)
+    z3=a2.w3+b3
+    a3=softmax(z3)
