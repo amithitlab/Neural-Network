edd57e16715100f45f2a043e8d9ce79e8caf1ea6
Amit Singh <amit8564880158@gmail.com>
2018-11-15 23:51:12 +0530

    Update Neural Network.py

--- Neural Network.py
+++ Neural Network.py
@@ -8,13 +8,21 @@ Created on Sat Oct 27 23:22:42 2018
 import numpy as np
 import pandas as pd
 
-x,y,w1,b1,w2,b2,w3,b3
+
+x,y,w1,b1,w2,b2,w3,b3=None
 
 
+
+def sigmoid(z):
+    sig=1/(1+np.exp(-z))
+    return sig
+    
 def forward(x):
-    z1=x.w1+b
+    z1=np.dot(X,w1)+b
     a1=tanh(z1)
-    z2=a1.w2+b2
+    z2=np.dot(a1,w2)+b2
     a2=tanh(z2)
     z3=a2.w3+b3
-    a3=softmax(z3)
+    a3=sigmoid(z3)
+    
+    
